69c0bdf7476c241a8ed69554d60c369a7ddf5d51
YogurtSnek <pootissonic1808@gmail.com>
2022-08-26 11:24:31 -0700

    Hello World, I am committing my project.

---
+++ Cuenta/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cuenta
+{
+    public class Account
+    {
+        private double balance;
+
+        public Account(double balance)
+        {
+            this.balance = balance;
+        }
+
+      
+        public void deposit(double balance)
+        {
+            this.balance += balance;
+            Console.WriteLine("Your new balance after the deposit is: " + this.balance);
+        }
+
+        public void withdraw(double balance)
+        {
+            this.balance -= balance;
+            Console.WriteLine("Your new balance after the withdrawal is: " + this.balance);
+        }
+
+         public void state()
+         {
+            Console.WriteLine("The balance of this account is: " + balance);
+         }
+
+        private double getBalance() { return balance; }
+
+
+        private void setBalance(double balance)
+            { this.balance = balance; }
+
+       
+    }
+}
---
+++ Cuenta/TestAcc.cs
@@ -0,0 +1,16 @@
+﻿using Cuenta;
+
+namespace TestAcc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account(50);
+            a.state();
+            a.deposit(100.0);
+            a.withdraw(147.0);
+        }
+    }
+}
+
